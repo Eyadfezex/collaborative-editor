fc57d1a66fe537a7bbfea608ce424f651165fb9d
Eyad Ahmed <Eyad932004@outlook.com>
2025-02-06 20:21:44 +0200

    Enhance LiveblocksProvider to resolve users and mention suggestions using Clerk and document user actions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,9 +5,29 @@ import {
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
+import {
+  getClerkUsersWithIds,
+  getDocumentUsers,
+} from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: ClerkUser } = useUser();
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsersWithIds({ userIds });
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: ClerkUser?.emailAddresses[0].emailAddress as string,
+          text,
+        });
+        return roomUsers;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
